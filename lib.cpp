2a1af1759fe8cd5ccd1eef76342bb0ddf15cd199
ValentinaStella <91681272+ValentinaStella@users.noreply.github.com>
2021-12-01 15:35:00 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
-
+#include "prototipoconversione.h"
+char conversionelettere (char c){
+    char risultato;
+    bool cifra;
+    if(c>='A'&& c<='Z') {
+        risultato = c + 32;
+    }else if( c>='a' && c<='z'){
+        risultato=c-32;
+    } else if (('0'<= c)&& (c<='9')){
+        cifra= false;
+    }
+    return risultato;
+    }
